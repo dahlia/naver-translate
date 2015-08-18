c389d8bb232960abce24caf9dc99780a0ca2d4bc
Hong Minhee <hongminhee@member.fsf.org>
2015-08-19 04:04:39 +0900

    Add translateUrl

--- src/Language/Translate/Naver.hs
+++ src/Language/Translate/Naver.hs
@@ -1,14 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Language.Translate.Naver ( translate
                                 , translateMultiple
+                                , translateUrl
                                 ) where
 
 import Control.Lens ((^?))
 import Control.Monad (liftM)
 import Data.Aeson.Lens (key)
 import Data.Aeson.Types (Value(String))
-import Data.LanguageCodes (ISO639_1, language)
+import Data.LanguageCodes (ISO639_1(JA, KO), language)
 import Data.Text (Text, cons, intercalate, pack, snoc, splitOn, strip)
+import Network.URI (URI(URI), URIAuth(URIAuth), relativeTo, uriIsAbsolute)
 import Network.Wreq (FormParam((:=)), post, responseBody)
 import System.Random (getStdRandom, randomR)
 
@@ -46,3 +48,26 @@ translateMultiple sourceLanguage targetLanguage texts = do
     let bundle = intercalate (snoc (cons ' ' spliter) ' ') texts
     result <- translate sourceLanguage targetLanguage bundle
     return $ map strip $ splitOn spliter result
+
+-- | Get the url of the translated page.  Other languages than Korean
+-- ('KO') and Japanese ('JA') are unsupported, so they get 'Nothing'.
+translateUrl :: ISO639_1  -- ^ The language translated from
+             -> ISO639_1  -- ^ The language to translate to
+             -> URI       -- ^ The url of the page to translate
+             -> Maybe URI -- ^ The url of the translated page
+translateUrl sourceLanguage targetLanguage (URI scheme authority path query _) =
+    case (scheme, authority) of
+        ("http:", Just (URIAuth "" host port)) ->
+            let relUrl = getRelUrl host port
+                getUrl = Just . relativeTo relUrl
+            in case langPair of
+                (JA, KO) -> getUrl j2k
+                (KO, JA) -> getUrl k2j
+                _ -> Nothing
+        _ -> Nothing
+  where
+    langPair = (sourceLanguage, targetLanguage)
+    baseAuth = Just $ URIAuth "" "jptrans.naver.net" ""
+    j2k = URI "http:" baseAuth "/j2k_frame.php/korean/" "" ""
+    k2j = URI "http:" baseAuth "/webtrans.php/korean/" "" ""
+    getRelUrl host port = URI "" Nothing (host ++ port ++ path) query ""
